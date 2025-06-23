3c3b614d0507826bcb5539366dd49726b4ba6bbc
Ayelen <ayelenvegacaro@gmail.com>
2025-06-23 06:52:07 -0300

    1era a 3era prueba

---
+++ src/clock.c
@@ -0,0 +1,65 @@
+/*********************************************************************************************************************
+Copyright (c) 2025, María Ayelén Vega Caro <ayelenvegacaro@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*********************************************************************************************************************/
+
+/** @file  reloj.c
+ ** @brief
+ **/
+
+/* === Headers files inclusions ==================================================================================== */
+#include "clock.h"
+#include <stddef.h>
+#include <string.h>
+/* === Macros definitions ========================================================================================== */
+
+/* === Private data type declarations ============================================================================== */
+struct clock_s {
+    clock_time_t current_time;
+    bool valid;
+};
+
+/* === Private function declarations =============================================================================== */
+
+/* === Private variable definitions ================================================================================ */
+
+/* === Public variable definitions ================================================================================= */
+
+/* === Private function definitions ================================================================================ */
+
+/* === Public function implementation ============================================================================== */
+clock_t ClockCreate(uint16_t ticks_per_second) {
+    (void)ticks_per_second;
+    static struct clock_s self[1];
+    memset(self, 0, sizeof(struct clock_s));
+    self->valid = false;
+    return self;
+}
+bool ClockGetTime(clock_t self, clock_time_t * result) {
+    memcpy(result, &self->current_time, sizeof(clock_time_t));
+    return self->valid;
+}
+bool ClockSetTime(clock_t self, const clock_time_t * new_time) {
+    self->valid = true;
+    memcpy(&self->current_time, new_time, sizeof(clock_time_t));
+    return self->valid;
+}
+void ClockNewTick(clock_t self) {
+    self->current_time.time.seconds[0] = 1;
+}
+
+/* === End of documentation ======================================================================================== */
---
+++ test/test_clock.c
@@ -0,0 +1,97 @@
+/*********************************************************************************************************************
+Copyright (c) 2025, María Ayelén Vega Caro <ayelenvegacaro@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+
+PRUEBAS A REALIZAR
+Hora actual
+- Al inicializar el reloj esta en 00:00 y la hora es invalida
+- Tratar de setear el reloj con una hora válida
+- Probar que avance de 0 a 1 segundo
+- Probar que avance de 9 a 10 segundos
+- Probar que avance de 59 a 1 minuto
+- Probar que avance de 9:59 a 10 minutos
+- Probar que avance de 59:59 a 1 hora
+- Probar que avance de 9:59:59 a 10 horas
+- Probar que avance de 23:59:59 a 00:00:00
+- Probar GetTime con NULL como argumento
+- Tratar de setear la hora con valores invalidos y verificar que los rechace
+- Hacer una prueba con frecuencia de reloj diferente
+
+
+Alarma
+-Probar que al setear la alarma esta suene
+-Probar que al deshabilitar la alarma no suene
+-Probar que al posponer alarma suene despues de x tiempo
+-Hacer sonar la alarma y cancelarla hasta el dia siguiente
+
+*********************************************************************************************************************/
+
+/** @file  test_reloj.c
+ ** @brief
+ **/
+
+/* === Headers files inclusions ==================================================================================== */
+#include "unity.h"
+#include "clock.h"
+
+/* === Macros definitions ========================================================================================== */
+#define CLOCK_TICK_PER_SECONDS 5
+
+/* === Private data type declarations ============================================================================== */
+
+/* === Private function declarations =============================================================================== */
+void SimulateSeconds(clock_t clock, uint8_t seconds);
+
+/* === Private variable definitions ================================================================================ */
+
+/* === Public variable definitions ================================================================================= */
+
+/* === Private function definitions ================================================================================ */
+void SimulateSeconds(clock_t clock, uint8_t seconds) {
+    for (uint8_t i = 0; i < CLOCK_TICK_PER_SECONDS * seconds; i++) {
+        ClockNewTick(clock);
+    }
+}
+
+/* === Public function implementation ============================================================================== */
+// Al inicializar el reloj está en 00:00 y con hora invalida.
+void test_set_up_with_invalid_time(void) {
+    clock_time_t current_time = {.bcd = {1, 2, 3, 4, 5, 6}};
+    clock_t clock = ClockCreate(CLOCK_TICK_PER_SECONDS);
+    TEST_ASSERT_FALSE(ClockGetTime(clock, &current_time));
+    TEST_ASSERT_EACH_EQUAL_UINT8(0, current_time.bcd, 6);
+}
+// Al ajustar la hora el reloj queda en hora y es válida.
+void test_set_up_with_valid_time(void) {
+    static const clock_time_t new_time = {.time = {.seconds = {4, 5}, .minutes = {1, 5}, .hours = {1, 2}}};
+    clock_time_t current_time = {0};
+    clock_t clock = ClockCreate(CLOCK_TICK_PER_SECONDS);
+    TEST_ASSERT_TRUE(ClockSetTime(clock, &new_time));
+    TEST_ASSERT_TRUE(ClockGetTime(clock, &current_time));
+    TEST_ASSERT_EQUAL_INT8_ARRAY(new_time.bcd, current_time.bcd, 6);
+}
+// Después de n ciclos de reloj la hora avanza un segundo
+void test_clock_advance_one_second(void) {
+    clock_time_t current_time = {0};
+    static const clock_time_t expected_time = {.time = {.seconds = {1, 0}, .minutes = {0, 0}, .hours = {0, 0}}};
+    clock_t clock = ClockCreate(CLOCK_TICK_PER_SECONDS);
+    ClockSetTime(clock, &(clock_time_t){0});
+    SimulateSeconds(clock, 1);
+    ClockGetTime(clock, &current_time);
+    TEST_ASSERT_EQUAL_INT8_ARRAY(expected_time.bcd, current_time.bcd, 6);
+}
+/* === End of documentation ======================================================================================== */
